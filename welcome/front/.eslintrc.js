48a52d416dc430ca74b6a12da126e37a669ee5e7
ryokohbato <ryokohbato@gmail.com>
2022-05-06 22:02:15 +0900

    prettier/import orderの設定を分離

--- welcome/front/.eslintrc.js
+++ welcome/front/.eslintrc.js
@@ -1,27 +1,46 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  "extends": [
+  extends: [
     "react-app",
     "react-app/jest",
     "eslint:recommended",
     "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier",
   ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+    project: "./tsconfig.json",
+    sourceType: "module",
   },
-  "plugins": [
-    "react",
-    "@typescript-eslint"
-  ],
-  "rules": {
-  }
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "no-console": "error",
+    "@typescript-eslint/consistent-type-imports": ["error", { prefer: "type-imports" }],
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "parent", "sibling", "index", "object", "type"],
+        pathGroups: [
+          {
+            pattern: "@alias/**",
+            group: "type",
+            position: "after",
+          },
+        ],
+        alphabetize: {
+          order: "asc",
+        },
+        "newlines-between": "always",
+      },
+    ],
+  },
 };
