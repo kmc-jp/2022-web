bd569d46a9b2d3c83e625f48ede7886aefdb024e
ryokohbato <ryokohbato@gmail.com>
2022-05-06 22:22:26 +0900

    lint checkを通す

--- welcome/front/src/App.tsx
+++ welcome/front/src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
 
 function App() {
-  const ProjectRenderer: React.FC<{ index: number, projectName: string }> = (props) => {
-    return (
-      props.index < 0 ?
-      <p className="App-subtitle" style={{
-        left: `calc(max(0px, min(1px, 100vw - 1024px)) * ${30 * props.index})`,
-        top: `max(${5 * props.index}vh, ${48 * props.index}px)`,
-      }}>
+  const ProjectRenderer: React.FC<{ index: number; projectName: string }> = (props) => {
+    return props.index < 0 ? (
+      <p
+        className="App-subtitle"
+        style={{
+          left: `calc(max(0px, min(1px, 100vw - 1024px)) * ${30 * props.index})`,
+          top: `max(${5 * props.index}vh, ${48 * props.index}px)`,
+        }}
+      >
         {props.projectName}
-      </p> :
-      <p className="App-subtitle" style={{
-        left: `calc(max(0px, min(1px, 100vw - 1024px)) * ${30 * (props.index + 1.75)})`,
-        top: `min(${5 * (props.index + 2.5)}vh, ${48 * (props.index + 1.5)}px)`,
-      }}>
-      {props.projectName}
-    </p>
-    )
-  }
+      </p>
+    ) : (
+      <p
+        className="App-subtitle"
+        style={{
+          left: `calc(max(0px, min(1px, 100vw - 1024px)) * ${30 * (props.index + 1.75)})`,
+          top: `min(${5 * (props.index + 2.5)}vh, ${48 * (props.index + 1.5)}px)`,
+        }}
+      >
+        {props.projectName}
+      </p>
+    );
+  };
 
   const projects = [
     "Unityでゲームを作る",
@@ -28,28 +34,28 @@ function App() {
     "3DCG勉強会",
     "Rustを知ろう",
     "競技プログラミング練習会",
-    "Principles of Program Analysis 読み会"
-  ]
+    "Principles of Program Analysis 読み会",
+  ];
 
   return (
     <div className="App">
       <div className="App-container">
         <div>
-          {
-            projects.filter((x, index) => index <= 5 ).map((x, index) => {
-              return <ProjectRenderer key={index} index={index - 6} projectName={x}/>
-            })
-          }
+          {projects
+            .filter((x, index) => index <= 5)
+            .map((x, index) => {
+              return <ProjectRenderer key={index} index={index - 6} projectName={x} />;
+            })}
         </div>
         <div className="App-title">
           <h1>Webサービス勉強会2022</h1>
         </div>
         <div>
-          {
-            projects.filter((x, index) => index >= 6 ).map((x, index) => {
-              return <ProjectRenderer key={index} index={index + 1} projectName={x}/>
-            })
-          }
+          {projects
+            .filter((x, index) => index >= 6)
+            .map((x, index) => {
+              return <ProjectRenderer key={index} index={index + 1} projectName={x} />;
+            })}
         </div>
       </div>
     </div>
--- welcome/front/src/reportWebVitals.ts
+++ welcome/front/src/reportWebVitals.ts
@@ -1,8 +1,9 @@
-import { ReportHandler } from 'web-vitals';
+import type { ReportHandler } from "web-vitals";
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    // eslint-disable-next-line
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
